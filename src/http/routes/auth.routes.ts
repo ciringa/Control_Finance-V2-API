c9e3636cec68fd23c314a74adab187f90ab46274
ciringa <thierrird957468@gmail.com>
2024-09-27 22:51:00 -0300

    File:organization

--- src/http/routes/auth.routes.ts
+++ src/http/routes/auth.routes.ts
@@ -2,8 +2,9 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { loginAsGuestSchema, LoginSchema, ProfileSchema } from "../schemas/auth.schemas";
 import { ReturnProfile } from "../controlers/User/ProfileControler";
-import { AutheticateUser } from "../controlers/Auth/AuthenticaControler";
-import { LoginAsGuestControler } from "../controlers/Auth/LoginAsGuestControler";
+import { AutheticateUser } from "../controlers/Authenticate/AuthenticaControler";
+import { LoginAsGuestControler } from "../controlers/Authenticate/LoginAsGuestControler";
+
 
 export async function AutheticateRoutes(app:FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().patch("/login",LoginSchema,AutheticateUser)
